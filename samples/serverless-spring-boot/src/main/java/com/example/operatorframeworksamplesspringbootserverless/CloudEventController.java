3afc43c1d5f83859b51e96aabae5c3fa2d9534a0
csviri <csviri@gmail.com>
2021-10-20 17:22:50 +0200

    ping source working stuff

--- samples/serverless-spring-boot/src/main/java/com/example/operatorframeworksamplesspringbootserverless/CloudEventController.java
+++ samples/serverless-spring-boot/src/main/java/com/example/operatorframeworksamplesspringbootserverless/CloudEventController.java
@@ -1,9 +1,12 @@
 package com.example.operatorframeworksamplesspringbootserverless;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +15,8 @@ public class CloudEventController {
   private static final Logger log = LoggerFactory.getLogger(CloudEventController.class);
 
   @PostMapping("/event")
-  public void processEvent(@RequestBody String event) {
-    log.info(event);
+  public void processEvent(@RequestBody String event, @RequestHeader Map<String, String> headers) {
+    log.info("Body: {}; Headers: {}", event, headers);
   }
 
 }
